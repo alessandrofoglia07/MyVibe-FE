3dc22478005181dcea806dd2dc89e36504b15faa
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-05-07 20:28:59 +0200

    Updated signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { StyledEngineProvider } from '@mui/material/styles';
 import MainPage from './pages/MainPage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
@@ -8,14 +9,16 @@ import LoginPage from './pages/LoginPage';
 function App() {
     return (
         <div className='App'>
-            <Router>
-                <Routes>
-                    <Route path='/' element={<MainPage />} />
-                    <Route path='/login' element={<LoginPage />} />
-                    <Route path='/sign-up' element={<SignUpPage />} />
-                    <Route path='*' element={<h1>404</h1>} />
-                </Routes>
-            </Router>
+            <StyledEngineProvider injectFirst>
+                <Router>
+                    <Routes>
+                        <Route path='/' element={<MainPage />} />
+                        <Route path='/login' element={<LoginPage />} />
+                        <Route path='/sign-up' element={<SignUpPage />} />
+                        <Route path='*' element={<h1>404</h1>} />
+                    </Routes>
+                </Router>
+            </StyledEngineProvider>
         </div>
     );
 }
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Typography, TextField, Button, IconButton, Paper, Stack } from '@mui/material';
-import { StyledEngineProvider } from '@mui/material/styles';
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
 import '../styles/authPages.css';
@@ -14,6 +13,63 @@ const SignUpPage = () => {
         username: '',
         email: '',
         password: ''
+    });
+    const [usernameFocus, setUsernameFocus] = useState(false);
+    const [emailFocus, setEmailFocus] = useState(false);
+    const [passwordFocus, setPasswordFocus] = useState(false);
+
+    useEffect(() => {
+        if (usernameFocus) {
+            const redBall = document.getElementsByClassName('ball-red')[0];
+            redBall.classList.add('ball-red-focus');
+
+            const usernameInput = document.getElementById('username') as HTMLInputElement;
+
+            usernameInput.addEventListener('focusout', () => {
+                redBall.classList.remove('ball-red-focus');
+                redBall.classList.add('ball-red-outFocus');
+                setTimeout(() => {
+                    redBall.classList.remove('ball-red-outFocus');
+                }, 1000);
+                setUsernameFocus(false);
+            });
+        }
+    });
+
+    useEffect(() => {
+        if (emailFocus) {
+            const blueBall = document.getElementsByClassName('ball-blue')[0];
+            blueBall.classList.add('ball-blue-focus');
+
+            const emailInput = document.getElementById('email') as HTMLInputElement;
+
+            emailInput.addEventListener('focusout', () => {
+                blueBall.classList.remove('ball-blue-focus');
+                blueBall.classList.add('ball-blue-outFocus');
+                setTimeout(() => {
+                    blueBall.classList.remove('ball-blue-outFocus');
+                }, 1000);
+                setEmailFocus(false);
+            });
+        }
+    }, [emailFocus]);
+
+    useEffect(() => {
+        if (passwordFocus) {
+            const purpleBall = document.getElementsByClassName('ball-purple')[0];
+            purpleBall.classList.add('ball-purple-focus');
+
+            const passwordInput = document.getElementById('password') as HTMLInputElement;
+
+            passwordInput.addEventListener('focusout', () => {
+                purpleBall.classList.remove('ball-purple-focus');
+                purpleBall.classList.add('ball-purple-outFocus');
+                setTimeout(() => {
+                    purpleBall.classList.remove('ball-purple-outFocus');
+                }, 1000);
+                setPasswordFocus(false);
+            });
+        }
     });
 
     const handleChange = (id: number, value: string) => {
@@ -39,80 +95,91 @@ const SignUpPage = () => {
     return (
         <div id='SignUpPage' className='page signupPage'>
             <div id='bg'>
-                <div className='ball' />
-                <div className='ball' />
-                <div className='ball' />
+                <div className='ball ball-blue' />
+                <div className='ball ball-red' />
+                <div className='ball ball-purple' />
             </div>
-            <StyledEngineProvider injectFirst>
-                <Paper className='form'>
-                    <Typography variant='h4' className='title'>
-                        Sign Up
-                    </Typography>
-                    <form autoComplete='off' onSubmit={handleSubmit}>
-                        <Stack display='flex' alignItems='center'>
-                            <div className='inputContainer'>
-                                <Typography variant='h5' className='subtitle'>
-                                    Username
-                                </Typography>
-                                <TextField
-                                    autoFocus
-                                    type='text'
-                                    value={input.username}
-                                    onChange={(e) => {
-                                        handleChange(0, e.target.value);
-                                    }}
-                                    placeholder='Username'
-                                    variant='outlined'
-                                    className='input'
-                                />
-                            </div>
-                            <div className='inputContainer'>
-                                <Typography variant='h5' className='subtitle'>
-                                    Email
-                                </Typography>
-                                <TextField
-                                    type='email'
-                                    value={input.email}
-                                    onChange={(e) => {
-                                        handleChange(1, e.target.value);
-                                    }}
-                                    placeholder='Email'
-                                    variant='outlined'
-                                    className='input'
-                                />
-                            </div>
-                            <div className='inputContainer'>
-                                <Typography variant='h5' className='subtitle'>
-                                    Password
-                                </Typography>
-                                <TextField
-                                    type={showPassword ? 'text' : 'password'}
-                                    placeholder='Password'
-                                    variant='outlined'
-                                    className='input'
-                                    value={input.password}
-                                    onChange={(e) => {
-                                        handleChange(2, e.target.value);
-                                    }}
-                                    InputProps={{
-                                        endAdornment: (
-                                            <IconButton
-                                                onClick={() => {
-                                                    setShowPassword(!showPassword);
-                                                }}>
-                                                {input.password === '' ? null : showPassword ? <Visibility /> : <VisibilityOff />}
-                                            </IconButton>
-                                        )
-                                    }}
-                                />
-                            </div>
-                            <Button disableRipple variant='contained' className='submitBtn' type='submit'>
-                                Sign Up
-                            </Button>
-                        </Stack>
-                    </form>
-                </Paper>
-            </StyledEngineProvider>
+
+            <Paper className='form'>
+                <Typography variant='h4' className='title'>
+                    Sign Up
+                </Typography>
+                <form autoComplete='off' onSubmit={handleSubmit}>
+                    <Stack display='flex' alignItems='center'>
+                        <div className='inputContainer'>
+                            <Typography variant='h5' className='subtitle'>
+                                Username
+                            </Typography>
+                            <TextField
+                                id='username'
+                                autoFocus
+                                type='text'
+                                value={input.username}
+                                onChange={(e) => {
+                                    handleChange(0, e.target.value);
+                                }}
+                                placeholder='Username'
+                                variant='outlined'
+                                className='input'
+                                onFocus={() => {
+                                    setUsernameFocus(true);
+                                }}
+                            />
+                        </div>
+                        <div className='inputContainer'>
+                            <Typography variant='h5' className='subtitle'>
+                                Email
+                            </Typography>
+                            <TextField
+                                id='email'
+                                type='email'
+                                value={input.email}
+                                onChange={(e) => {
+                                    handleChange(1, e.target.value);
+                                }}
+                                placeholder='Email'
+                                variant='outlined'
+                                className='input'
+                                onFocus={() => {
+                                    setEmailFocus(true);
+                                }}
+                            />
+                        </div>
+                        <div className='inputContainer'>
+                            <Typography variant='h5' className='subtitle'>
+                                Password
+                            </Typography>
+                            <TextField
+                                id='password'
+                                type={showPassword ? 'text' : 'password'}
+                                placeholder='Password'
+                                variant='outlined'
+                                className='input'
+                                value={input.password}
+                                onChange={(e) => {
+                                    handleChange(2, e.target.value);
+                                }}
+                                InputProps={{
+                                    endAdornment: (
+                                        <IconButton
+                                            onClick={() => {
+                                                setShowPassword(!showPassword);
+                                            }}>
+                                            {input.password === '' ? null : showPassword ? <Visibility /> : <VisibilityOff />}
+                                        </IconButton>
+                                    )
+                                }}
+                                onFocus={() => {
+                                    setPasswordFocus(true);
+                                }}
+                            />
+                        </div>
+                        <Button disableRipple variant='contained' className='submitBtn' type='submit'>
+                            Sign Up
+                        </Button>
+                    </Stack>
+                </form>
+            </Paper>
         </div>
     );
 };
