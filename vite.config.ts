81e38eda2290dcd36599992e8e576b5f4c40b04a
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-05-23 21:17:04 +0200

    Completed migration from CRA to Vite

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), viteTsConfigPaths(), svgrPlugin()],
+    server: {
+        port: 3000,
+    }
+});
