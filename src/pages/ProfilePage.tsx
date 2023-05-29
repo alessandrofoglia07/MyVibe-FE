82c43b76311f36c77c0d2ef4a6b21a1dd0a6cee1
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-05-29 16:10:57 +0200

    Added loading progress to profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -4,7 +4,7 @@ import '../style/ProfilePage.scss';
 import Navbar from '../components/navbar';
 import { useParams } from 'react-router-dom';
 import authAxios from '../api/authAxiosApi';
-import { Avatar, Typography, Stack, Button } from '@mui/material';
+import { Avatar, Typography, Stack, Button, CircularProgress } from '@mui/material';
 import PersonIcon from '@mui/icons-material/Person';
 import Post from '../components/Post';
 import formatDate from '../utils/formatDate';
@@ -29,7 +29,6 @@ const ProfilePage = () => {
     useEffect(() => {
         (async () => {
             try {
-                setLoading(true);
                 await getUserData();
                 await getPosts();
             } catch (err) {
@@ -65,66 +64,72 @@ const ProfilePage = () => {
             </header>
             <main>
                 {/* TODO Fix this */}
-                <div className='top'>
-                    <Button className='avatarContainer' disableRipple href={`/profile/${username}`}>
-                        <Avatar className='avatar' src='/assets/pfp-placeholder.png' alt='pfp'>
-                            <PersonIcon sx={{ fontSize: '80px' }} />
-                        </Avatar>
-                    </Button>
-                    <div>
-                        <Typography component='p' className='username'>
-                            {username}
-                        </Typography>
-                        <Typography component='p' className='email'>
-                            {user?.email}
-                        </Typography>
-                    </div>
-                </div>
-                {user?.info.firstName && user?.info.lastName ? (
-                    <Typography component='p' className='fullName'>
-                        {user?.info.firstName + ' ' + user?.info.lastName}
-                    </Typography>
-                ) : null}
-                <Stack direction='row' spacing={3} className='numericStats'>
-                    <Typography component='p' className='following'>
-                        Following <br /> {user?.followingIDs.length}
-                    </Typography>
-                    <Typography component='p' className='followers'>
-                        Followers <br /> {user?.followersIDs.length}
-                    </Typography>
-                </Stack>
-                <Typography component='p' className='createdAt'>
-                    Joined {formatDate(user?.createdAt as Date)}
-                </Typography>
-                <Typography component='p' className='bio'>
-                    {user?.info.bio || 'No bio yet.'}
-                </Typography>
-                <div className='posts'>
-                    <Typography component='p' className='postsTitle'>
-                        Featured Posts
-                    </Typography>
-                    {posts.length > 0 ? (
-                        <Stack spacing={2} width='80%'>
-                            {posts.map((post) => (
-                                <Post
-                                    key={post._id}
-                                    _id={post._id}
-                                    author={post.author}
-                                    authorUsername={post.authorUsername}
-                                    content={post.content}
-                                    date={post.createdAt}
-                                    likes={post.likes.length}
-                                    liked={post.liked}
-                                    comments={post.comments.length}
-                                />
-                            ))}
+                {loading ? (
+                    <CircularProgress className='loading' />
+                ) : (
+                    <>
+                        <div className='top'>
+                            <Button className='avatarContainer' disableRipple href={`/profile/${username}`}>
+                                <Avatar className='avatar' src='/assets/pfp-placeholder.png' alt='pfp'>
+                                    <PersonIcon sx={{ fontSize: '80px' }} />
+                                </Avatar>
+                            </Button>
+                            <div>
+                                <Typography component='p' className='username'>
+                                    {username}
+                                </Typography>
+                                <Typography component='p' className='email'>
+                                    {user?.email}
+                                </Typography>
+                            </div>
+                        </div>
+                        {user?.info.firstName && user?.info.lastName ? (
+                            <Typography component='p' className='fullName'>
+                                {user?.info.firstName + ' ' + user?.info.lastName}
+                            </Typography>
+                        ) : null}
+                        <Stack direction='row' spacing={3} className='numericStats'>
+                            <Typography component='p' className='following'>
+                                Following <br /> {user?.followingIDs.length}
+                            </Typography>
+                            <Typography component='p' className='followers'>
+                                Followers <br /> {user?.followersIDs.length}
+                            </Typography>
                         </Stack>
-                    ) : (
-                        <Typography variant='h6' className='noPostsText'>
-                            No posts here...
+                        <Typography component='p' className='createdAt'>
+                            Joined {formatDate(user?.createdAt as Date)}
                         </Typography>
-                    )}
-                </div>
+                        <Typography component='p' className='bio'>
+                            {user?.info.bio || 'No bio yet.'}
+                        </Typography>
+                        <div className='posts'>
+                            <Typography component='p' className='postsTitle'>
+                                Featured Posts
+                            </Typography>
+                            {posts.length > 0 ? (
+                                <Stack spacing={2} width='80%'>
+                                    {posts.map((post) => (
+                                        <Post
+                                            key={post._id}
+                                            _id={post._id}
+                                            author={post.author}
+                                            authorUsername={post.authorUsername}
+                                            content={post.content}
+                                            date={post.createdAt}
+                                            likes={post.likes.length}
+                                            liked={post.liked}
+                                            comments={post.comments.length}
+                                        />
+                                    ))}
+                                </Stack>
+                            ) : (
+                                <Typography variant='h6' className='noPostsText'>
+                                    No posts here...
+                                </Typography>
+                            )}
+                        </div>
+                    </>
+                )}
             </main>
         </div>
     );
