9cb81f0f274571dd6a249ce16c46e9a1872f7c40
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-06-21 20:43:18 +0200

    Added PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+import manifest from './manifest.json';
+
+const pwaConfig: Partial<VitePWAOptions> = {
+    manifest,
+    manifestFilename: 'manifest.json',
+    includeAssets: ['favicon.ico', 'robots.txt'],
+    workbox: {
+        globPatterns: ['**/*.{js,css,html,json,ico,png,jpg,jpeg,svg}'],
+    }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), viteTsConfigPaths(), svgrPlugin()],
+    plugins: [react(), viteTsConfigPaths(), svgrPlugin(), VitePWA(pwaConfig)],
     server: {
-        port: 3000,
+        port: 4173,
         open: true,
         host: true,
     }
